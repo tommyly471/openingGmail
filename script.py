61d575146cb641db39a9c89bb99ce24d82e64ad7
tommyly471 <tommyly471.gmail.com>
2024-09-08 23:39:03 -0500

    added second url

--- script.py
+++ script.py
@@ -6,3 +6,5 @@ gmail_url = "https://mail.google.com/"
 
 # Use the webbrowser module to open Gmail with Chrome
 webbrowser.get(f'"{chrome_path}" %s').open(gmail_url)
+
+webbrowser.get(f'"{chrome_path}" --profile-directory="Profile 1" %s').open(gmail_url)
